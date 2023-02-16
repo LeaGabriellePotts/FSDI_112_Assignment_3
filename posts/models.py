869a9226fdd0e1563bb6e591d98be5f5d7bd9faf
Lea P <lea.potts.ord@gmail.com>
2023-02-15 23:05:58 -0500

    First Commit

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Post(models.Model): # inheritance
     title = models.CharField(max_length=128) # composition
@@ -9,3 +9,6 @@ class Post(models.Model): # inheritance
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("detail", args=[self.id])
--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,12 @@
-from django.urls import re_path
-from .views import PostListView
+from django.urls import path
+from .views import (
+    PostListView, 
+    PostCreateView,
+    PostDetailView
+)
 
 urlpatterns = [
     path("", PostListView.as_view(), name="list"),
+    path("new/", PostCreateView.as_view(), name="new"),
+    path("<int:pk>/", PostDetailView.as_view(), name="detail"),
 ]
